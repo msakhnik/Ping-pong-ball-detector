8a77af2fea81437a88010a4e71d4b0d75db6c46e
Misha Sakhnik <kskster@gmail.com>
2012-03-28 16:17:42 +0300

    +Add header with GPL info

--- src/BallDetector.h
+++ src/BallDetector.h
@@ -1,9 +1,23 @@
-/* 
- * File:   BallDetector.h
- * Author: morwin
- *
- * Created on 12 березня 2012, 15:26
- */
+//
+// BallDetector.h
+//
+//     Created: 20.03.2012
+//      Author: Sakhnik
+//
+// This file is part of Ball Detector program.
+//
+//  Ball Detector program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Asf Player License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+//  Ball Detector program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with  Ball Detector program.  If not, see <http://www.gnu.org/licenses/>
 
 #pragma once
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,24 @@
+//
+// main.cpp
+//
+//     Created: 20.03.2012
+//      Author: Sakhnik
+//
+// This file is part of Ball Detector program.
+//
+//  Ball Detector program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Asf Player License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+//  Ball Detector program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with  Ball Detector program.  If not, see <http://www.gnu.org/licenses/>
+
 #include "BallDetector.h"
 #include <iostream>
 #include <getopt.h>
